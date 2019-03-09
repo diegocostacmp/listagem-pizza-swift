b13d4ba2fd32f6c47ceb8e58185f3f0208d41319
Aluno <aluno@imac-de-suporte-79.local>
2019-03-09 11:36:15 -0300

    Initial commit

--- MyPhome/ViewController.swift
+++ MyPhome/ViewController.swift
@@ -8,15 +8,34 @@
 
 import UIKit
 
+// classe que controla a lógica e estado de uma tela
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("telaMain foi carregada")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        print("a tela 1 está pronta mas ainda não visível")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        print("tela 1 já visível")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("a tela 1 desaparecerá em breve")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("a tela 1 já desapareceu")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        print("")
         // Dispose of any resources that can be recreated.
     }
 
