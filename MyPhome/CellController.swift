0e7e51319a346184435ce30baf8023ebf1a37789
Aluno <aluno@imac-de-suporte-79.local>
2019-03-23 14:47:21 -0300

    .

---
+++ MyPhome/CellController.swift
@@ -0,0 +1,19 @@
+//
+//  CellController.swift
+//  MyPhome
+//
+//  Created by Aluno on 22/03/2019.
+//  Copyright © 2019 kcfg23. All rights reserved.
+//
+
+import UIKit
+
+class CellController: UITableViewCell
+{
+    
+    @IBOutlet weak var vrImagemView: UIImageView!
+    
+    @IBOutlet weak var vrLabel: UILabel!
+    
+    
+}
